9eee9d85fe2262806b81db13222fc4ac5a9f81df
Manish Roy <manish.11612939@lpu.in>
2022-11-13 21:02:39 +0530

    added LiveScores.js

---
+++ src/appPages/LiveScores.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function LiveScores() {
+
+    return (
+        <div className='my-5 border-top-0 border-left-0 border-right-0 border--accent pt-3 pb-2 px-4 bg--secondary'>
+            <h3 className=''>TODAY'S MATCHES</h3>
+        </div>
+    )
+}
+
+export default LiveScores
