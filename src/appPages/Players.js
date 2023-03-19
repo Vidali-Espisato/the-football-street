0c11690325a713c28f532ad46320252680412b68
Manish Roy <manish.11612939@lpu.in>
2023-03-19 21:02:39 +0530

    added Players.js

---
+++ src/appPages/Players.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import UnderConstruction from '../assets/UnderConstruction.png'
+
+
+function Players() {
+  return (
+    <div>
+      <img src={ UnderConstruction } />
+    </div>
+  )
+}
+
+export default Players
